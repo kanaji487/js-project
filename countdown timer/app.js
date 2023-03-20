1b71e04e4ad54d5f3e17a4e0781c4eaf8332c56f
Auttawut <67725533+kanaji487@users.noreply.github.com>
2023-03-20 15:09:59 +0700

    Add files via upload

---
+++ countdown timer/app.js
@@ -0,0 +1,32 @@
+(() => {
+    const second = 1000;
+    const minute = second*60;
+    const hour = minute*60;
+    const day = hour*24;
+
+    function setElementInnerText(id, text) {
+        const element = document.getElementById(id);
+        element.innerText = text;
+    }
+
+    function countdown() {
+        const now = new Date().getTime();
+        const newYear = new Date('December 31, 2023 23:59:59').getTime();
+        const unixTimeLeft = newYear - now;
+
+        setElementInnerText('days', Math.floor(unixTimeLeft / day));
+;
+        setElementInnerText('hours', Math.floor(unixTimeLeft % day / hour));
+
+        setElementInnerText('minutes', Math.floor(unixTimeLeft % hour / minute));
+
+        setElementInnerText('seconds', Math.floor(unixTimeLeft % minute / second));
+    }
+
+    function run() {
+        countdown();
+        setInterval(countdown, second);
+    }
+    run();
+})();
+
